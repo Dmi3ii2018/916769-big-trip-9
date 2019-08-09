4f152deafa1a956fb858538712f9a09bf6209170
Dmitrii Alekseev <dalekseev2017@gmail.com>
2019-08-10 02:10:34 +0500

    Создл функции отрисовки компонентов

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import {createMenuTemplate} from "../src/components/menu.js";
+import {createFilterTemplate} from "../src/components/filter.js";
+import {createCardTemplate} from "../src/components/card.js";
+import {createEventEditorTemplate} from "../src/components/edit-form.js";
+import {createSortTemplate} from "../src/components/sort.js";
+import {createTripinfoTemplate} from "../src/components/trip-info.js";
+
+const renderElement = function (className, place, creatingFunction, count = 1) {
+  for (let i = 0; i < count; i++) {
+    let menuContainer = document.querySelector(className);
+
+    menuContainer.insertAdjacentHTML(place, creatingFunction);
+  }
+};
+
+renderElement(`.trip-main__trip-info`, `afterbegin`, createTripinfoTemplate());
+renderElement(`.trip-main__trip-controls`, `beforeend`, createMenuTemplate());
+renderElement(`.trip-main__trip-controls`, `beforeend`, createFilterTemplate());
+renderElement(`.trip-events`, `beforeend`, createSortTemplate());
+renderElement(`.trip-events`, `beforeend`, createEventEditorTemplate());
+renderElement(`.trip-events`, `beforeend`, createCardTemplate(), 3);
