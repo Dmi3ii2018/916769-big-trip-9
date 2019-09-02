2139e02c4b740e6d04cad7014b75a91c73edcdb4
Dmitrii Alekseev <dalekseev2017@gmail.com>
2019-09-02 11:25:31 +0500

    Рендер трипинфо

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,34 +1,53 @@
-export const createSortTemplate = ({sortItem, checked}) => {
-  return `
-  <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-  <span class="trip-sort__item  trip-sort__item--day">Day</span>
+import {createElement} from "../utils.js";
 
-  <div class="trip-sort__item  trip-sort__item--event">
-    <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" ${checked ? `` : `checked`}>
-    <label class="trip-sort__btn" for="sort-event">${sortItem[Math.floor(Math.random() * 4)]}</label>
-  </div>
+export class Sort {
+  constructor({sortItem, checked}) {
+    this._sortItem = sortItem;
+    this._checked = checked;
+    this._element = null;
+  }
 
-  <div class="trip-sort__item  trip-sort__item--time">
-    <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" ${checked ? `checked` : ``}>
-    <label class="trip-sort__btn" for="sort-time">
-    ${sortItem[Math.floor(Math.random() * 4)]}
-      <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-        <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-      </svg>
-    </label>
-  </div>
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
 
-  <div class="trip-sort__item  trip-sort__item--price">
-    <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-    <label class="trip-sort__btn" for="sort-price">
-    ${sortItem[Math.floor(Math.random() * 4)]}
-      <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-        <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-      </svg>
-    </label>
-  </div>
+  removeElement() {
+    this._element = null;
+  }
 
-  <span class="trip-sort__item  trip-sort__item--offers">${sortItem[Math.floor(Math.random() * 4)]}</span>
-</form>
-  `;
-};
+  getTemplate() {
+    return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+      <span class="trip-sort__item  trip-sort__item--day">Day</span>
+
+      <div class="trip-sort__item  trip-sort__item--event">
+        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" ${this._checked ? `` : `checked`}>
+        <label class="trip-sort__btn" for="sort-event">${this._sortItem[Math.floor(Math.random() * 4)]}</label>
+      </div>
+
+      <div class="trip-sort__item  trip-sort__item--time">
+        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" ${this._checked ? `checked` : ``}>
+        <label class="trip-sort__btn" for="sort-time">
+        ${this._sortItem[Math.floor(Math.random() * 4)]}
+          <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+            <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+          </svg>
+        </label>
+      </div>
+
+      <div class="trip-sort__item  trip-sort__item--price">
+        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+        <label class="trip-sort__btn" for="sort-price">
+        ${this._sortItem[Math.floor(Math.random() * 4)]}
+          <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+            <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+          </svg>
+        </label>
+      </div>
+
+      <span class="trip-sort__item  trip-sort__item--offers">${this._sortItem[Math.floor(Math.random() * 4)]}</span>
+    </form>`;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,33 +1,42 @@
 import {createMenuTemplate} from "../src/components/menu.js";
-import {createFilterTemplate} from "../src/components/filter.js";
+import {Filter} from "../src/components/filter.js";
 import {createTripDayTemplate} from "../src/components/trip-day-item.js";
 import {createEventEditor} from "../src/components/edit-form.js";
 import {createTripEvent} from "../src/components/trip-event.js";
-import {createSortTemplate} from "../src/components/sort.js";
-import {createTripinfoTemplate} from "../src/components/trip-info.js";
+import {Sort} from "../src/components/sort.js";
+import {TripInfo} from "../src/components/trip-info.js";
 import {createRoutePoint, eventsList, createMenu, createFilter, createRouteInfo, createSort} from "../src/components/data.js";
+import {render, Position} from "../src/utils.js";
 
-const renderElement = (className, place, dataStructure, template, count = 1) => {
-  let menuContainer = document.querySelector(className);
+const filterContainer = document.querySelector(`.trip-main__trip-controls`);
+const sortContainer = document.querySelector(`.trip-events`);
+const tripInfoContainer = document.querySelector(`.trip-main`);
 
-  menuContainer.insertAdjacentHTML(place, new Array(count)
-    .fill(``)
-    .map(dataStructure)
-    .map(template)
-    .join(``));
+const renderFilter = (filterData) => {
+  const filter = new Filter(filterData);
+  render(filterContainer, filter.getElement(), Position.BEFOREEND);
 };
 
-const renderEventPoint = (className, place, dataStructure, template) => {
-  let menuContainer = document.querySelector(className);
-  let eventData = dataStructure.map(((it, i) => i === 0 ? `` : template(it))).join(``);
-  menuContainer.insertAdjacentHTML(place, eventData);
+const renderSort = (sortData) => {
+  const sort = new Sort(sortData);
+  render(sortContainer, sort.getElement(), Position.AFTERBEGIN);
 };
 
-renderElement(`.trip-main`, `afterbegin`, createRouteInfo, createTripinfoTemplate);
-renderElement(`.trip-main__trip-controls`, `beforeend`, createMenu, createMenuTemplate);
-renderElement(`.trip-main__trip-controls`, `beforeend`, createFilter, createFilterTemplate);
-renderElement(`.trip-events`, `beforeend`, createSort, createSortTemplate);
-renderElement(`.trip-events`, `beforeend`, createRoutePoint, createTripDayTemplate);
-renderElement(`.trip-events__list`, `afterbegin`, () => eventsList[0], createEventEditor);
-renderEventPoint(`.trip-events__list`, `beforeend`, eventsList, createTripEvent);
+const renderTripInfo = (tripInfoData) => {
+  const tripInfo = new TripInfo(tripInfoData);
+  render(tripInfoContainer, tripInfo.getElement(), Position.AFTERBEGIN);
+};
+
+renderFilter(createFilter());
+renderSort(createSort());
+renderTripInfo(createRouteInfo());
+
+
+// renderElement(`.trip-main`, `afterbegin`, createRouteInfo, createTripinfoTemplate);
+// renderElement(`.trip-main__trip-controls`, `beforeend`, createMenu, createMenuTemplate);
+// renderElement(`.trip-main__trip-controls`, `beforeend`, createFilter, createFilterTemplate);
+// renderElement(`.trip-events`, `beforeend`, createSort, createSortTemplate);
+// renderElement(`.trip-events`, `beforeend`, createRoutePoint, createTripDayTemplate);
+// renderElement(`.trip-events__list`, `afterbegin`, () => eventsList[0], createEventEditor);
+// renderEventPoint(`.trip-events__list`, `beforeend`, eventsList, createTripEvent);
 
